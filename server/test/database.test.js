b8b249a2e4d9515f8543c5d48feab98457424e96
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-09-08 00:22:09 +0300

    Created test functions for post  database queries and routes Relates #19

--- server/test/database.test.js
+++ server/test/database.test.js
@@ -8,23 +8,26 @@ afterAll(() => connection.end());
 
 describe('Testing posts queries', () => {
   test('test adding new post', () => {
-    const expeted = {
-      title: 'This is a test post',
-      content: 'this is test content',
+    const expected = {
+      content: 'This is a test post',
+      userId: 1,
+      title: 'his is a test post',
       category: 'all',
       type: 'text',
-      userId: 1,
       imageUrl: 'https://media.sproutsocial.com/uploads/2017/01/Instagram-Post-Ideas.png',
     };
 
-    return queries.postsQueries.addPostQuery(expeted).then((data) => {
-      expect(data.title).toBe(expeted.title);
-      expect(data.content).toBe(expeted.content);
-      expect(data.type).toBe(expeted.type);
-      expect(data.category).toBe(expeted.category);
-      expect(data.userId).toBe(expeted.userId);
-      expect(data.imageUrl).toBe(expeted.imageUrl);
-    });
+    return queries.postsQueries.addPostQuery(expected)
+      .then((data) => data.rows[0])
+      .then((result) => {
+        console.log(result);
+        expect(result.title).toBe(expected.title);
+        expect(result.content).toBe(expected.content);
+        expect(result.type).toBe(expected.type);
+        expect(result.category).toBe(expected.category);
+        expect(result['user_id']).toBe(expected.userId);
+        expect(result['image_url']).toBe(expected.imageUrl);
+      });
   });
 
   test('test returning all posts', () => queries.postsQueries.allPostsQuery()
